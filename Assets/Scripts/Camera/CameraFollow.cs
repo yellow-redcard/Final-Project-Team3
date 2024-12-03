36813fe1289dcaca7ebcd7e4fae26a71b4fee15e
yellow-redcard <iclever0507@gmail.com>
2024-12-03 14:52:06 +0900

    [Fix]Manager

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CameraFollow : MonoBehaviour
     public Transform slime;
     public float cameraPositionZ = -10f;
 
-    void LateUpdate()
+    void FixedUpdate()
     {
         Vector3 position = slime.position;
         Vector3 cameraPosition = new Vector3(position.x, position.y, cameraPositionZ);
