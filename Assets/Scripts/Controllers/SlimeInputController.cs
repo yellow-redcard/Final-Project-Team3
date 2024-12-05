c799f84ef3176b9eb02a18f15213f7272533f77d
yellow-redcard <iclever0507@gmail.com>
2024-12-05 20:34:23 +0900

    [Update]Slime Animation

--- Assets/Scripts/Controllers/SlimeInputController.cs
+++ Assets/Scripts/Controllers/SlimeInputController.cs
@@ -3,9 +3,36 @@ using UnityEngine.InputSystem;
 
 public class SlimeInputController : TopDownController
 {
+    public Animator moveAnimator;
     public void OnMove(InputValue value)
     {
         Vector2 moveInput = value.Get<Vector2>().normalized;
         CallMoveEvent(moveInput);
+        MoveAnimation(value);
+    }
+    private void MoveAnimation(InputValue value)
+    {
+        Vector2 move = value.Get<Vector2>();
+        if (move.x > 0)
+        {
+            moveAnimator.Play("MoveRight");
+        }
+        else if (move.x < 0)
+        {
+            moveAnimator.Play("MoveLeft");
+        }
+        else if (move.y > 0)
+        {
+            moveAnimator.Play("MoveUp");
+        }
+        else if (move.y < 0)
+        {
+            moveAnimator.Play("MoveDown");
+        }
+        else
+        {
+            moveAnimator.Play("Idle");
+        }
     }
 }
+
