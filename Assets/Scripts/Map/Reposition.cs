969afb2a83b051e14eb925c733a1e04bcac08a6c
hyobeen12 <aszx8683@naver.com>
2024-12-30 17:23:49 +0900

    [fix] Reposition

--- Assets/Scripts/Map/Reposition.cs
+++ Assets/Scripts/Map/Reposition.cs
@@ -13,24 +13,29 @@ public class Reposition : MonoBehaviour
         float diffY = Mathf.Abs(playerPos.y - myPos.y);
 
         Vector3 playerDir = GameManager.Instance.playerMovement.moveDirection;
-        float dirX = playerDir.x < 0 ? -1 : (playerDir.x > 0 ? 1 : 0); //플레이어의 X값이 +인지 -인지에 따라 -1 또는 1로
-        float dirY = playerDir.y < 0 ? -1 : (playerDir.y > 0 ? 1 : 0);
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
+
+        float diffx = Mathf.Abs(dirX);
+        float diffy = Mathf.Abs(dirY);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
 
         switch (transform.tag)
         {
             case "Ground":
-                if (Mathf.Approximately(diffX, diffY))
+                if (diffX > diffY)
                 {
-                    transform.Translate(Vector3.up * dirY * 80);
-                    transform.Translate(Vector3.right * dirX * 80);
-                }
-                else if (diffX > diffY)
-                {
-                    transform.Translate(Vector3.right * dirX * 80);
+                    transform.Translate(dirX * 80, 0, 0);
                 }
                 else if (diffX < diffY)
                 {
-                    transform.Translate(Vector3.up * dirY * 80);
+                    transform.Translate(0, dirY * 80, 0);
+                }
+                else
+                {
+                    transform.Translate(dirX * 80, dirY * 80, 0);
                 }
                 break;
         }
