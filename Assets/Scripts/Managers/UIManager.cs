f02f2e3587fa580a23c72c38932ccbe89a915be2
yellow-redcard <iclever0507@gmail.com>
2025-01-02 21:47:51 +0900

    [Fix]Dead
    Dead와 관련된 메서드 수정

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -93,7 +93,14 @@ public class UIManager : MonoBehaviour, IManager
         uiList.Remove(go);
         Destroy(go.canvas.gameObject);
     }
-
+    public void CloseUI()
+    {
+        foreach (UIBase ui in uiList)
+        {
+            Destroy(ui.canvas.gameObject);
+        }
+        uiList.Clear();
+    }
     public void ShowLevelUpUI(List<SkillData> upgradeableSkills)
     {
         if (levelUpUIInstance == null)
@@ -113,4 +120,3 @@ public class UIManager : MonoBehaviour, IManager
         Debug.Log($"[UIManager] LevelUpUI 활성화. 버튼 개수: {upgradeableSkills.Count}");
     }
 }
-
