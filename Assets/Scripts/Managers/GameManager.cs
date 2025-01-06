cf2ed5ab6f6b1b5979558f6229d6450c397f445e
yellow-redcard <iclever0507@gmail.com>
2025-01-06 11:04:23 +0900

    [Fix]
    UI 수정

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -34,6 +34,9 @@ public class GameManager : MonoSingleton<GameManager>
         skillManager.init();
         player = GameObject.FindGameObjectWithTag(playerTag).transform;
         uiManager.Show<KillUI>();
+        uiManager.Show<ExpUI>();
+        uiManager.Show<SlimeHPUI>();
+        uiManager.Show<LevelUI>();
         StartCoroutine(skillManager.AutoFireSkills());
     }
 
