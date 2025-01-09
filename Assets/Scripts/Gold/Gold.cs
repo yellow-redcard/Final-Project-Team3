b4fc246a57534631e6139cf0b0ce9add396a3ccc
hyobeen12 <aszx8683@naver.com>
2025-01-09 11:13:06 +0900

    [add] Get Gold
    플레이어가 골드에 충돌시 골드가 오르고 골드오브젝트는 파괴됩니다

---
+++ Assets/Scripts/Gold/Gold.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : MonoBehaviour
+{
+    // 골드 데이터를 관리하는 변수
+    public static int goldCount = 0;
+
+    // 트리거 충돌 감지
+    private void OnTriggerEnter(Collider other)
+    {
+        // 충돌한 오브젝트가 플레이어인지 확인
+        if (other.CompareTag("Player"))
+        {
+            // 골드 데이터 증가
+            goldCount += 1;
+
+            // 골드 오브젝트 제거
+            Destroy(gameObject);
+
+            // 디버그 메시지 출력 (테스트용)
+            Debug.Log("골드를 획득했습니다! 현재 골드: " + goldCount);
+        }
+    }
+}
